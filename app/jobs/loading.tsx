114ae92f83707460737ba0211ac3135c1bf4048e
madisonlewiss <madison.lewis1@outlook.com>
2026-05-02 00:02:49 -0400

    Create loading.tsx

---
+++ app/jobs/loading.tsx
@@ -0,0 +1,16 @@
+export default function JobsLoading() {
+  return (
+    <div className="max-w-7xl mx-auto px-4 py-8">
+      <div className="flex gap-8">
+        <div className="w-72 flex-shrink-0">
+          <div className="h-96 bg-[#E2DDD8] rounded-lg animate-pulse" />
+        </div>
+        <div className="flex-1 flex flex-col gap-4">
+          {[...Array(5)].map((_, i) => (
+            <div key={i} className="h-32 bg-[#E2DDD8] rounded-lg animate-pulse" />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
